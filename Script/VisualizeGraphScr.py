276011f086134022eb3a944c6932a13cad301c97
mbbrinkman <oko018@my.utsa.edu>
2021-07-28 09:29:18 -0500

    fixed formatting

--- Script/VisualizeGraphScr.py
+++ Script/VisualizeGraphScr.py
@@ -1,25 +1,24 @@
 # Add path to the Class folder. All classes can now be imported directly
 from sys import path
 from os import getcwd
-import matplotlib
 path.append(getcwd() + "/../Class")
 from CurriculumGraph import CurriculumGraph
 
-# import matplotlib
+# import plotting utils
+import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
-#print(getcwd())
 
 # Load additional libraries
 import networkx as nx
 import numpy as np
 
 # Instantiate the class and useful variables.
-#
 cg = CurriculumGraph(r'../Data/FrequencyListOfTopics.xlsx', '1073')
 Adjacency = cg.Adjacency
 LeastDistance = cg.LeastDistance
 Route = cg.Route
+
 # Show path from first to second argument
 cg.PrintPath('0','3')
 
