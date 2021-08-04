9c967d98caf7b8640009238b829962b65109c3d4
mbbrinkman <oko018@my.utsa.edu>
2021-08-04 16:42:49 -0500

    Fixed 1043 not appearing in OPUS.  Also removed hacky code.

--- Class/CurriculumDB.py
+++ Class/CurriculumDB.py
@@ -9,7 +9,7 @@ class CurriculumDB:
     cursor = None
     edgesnumbers = []
     Lessons = []
-    LessonsClasses = None
+    LessonsClasses = []
     CoursesDescriptions = None
     def __init__(self, excel_file, serverip, databasename, username, password):
         cg = CurriculumGraph.CurriculumGraph(excel_file)
@@ -19,12 +19,24 @@ class CurriculumDB:
             self.edgesnumbers.append(int(i.replace("Edges", "")))
         Edges = cg.GetEdges()
         Edges = Edges.to_numpy()
-        for i in range(0,int(Edges.size/3)):
+        for i in range(0,len(Edges[:,0])):
             self.Lessons.append(Edges[i][0])
         self.LessonsClasses = cg.GetAliases()
-        for i in range(0,int(self.LessonsClasses.size/3)):
-            self.LessonsClasses[i][1] = int(self.LessonsClasses[i][1].replace("MAT",""))
+        self.GenLessonsClasses()
         self.CoursesDescriptions = pd.read_excel(cg.excel_file, sheet_name = "CourseNamesNarratives").to_numpy()
+
+    def GenLessonsClasses(self):
+        for i in range(0,len(self.LessonsClasses[:,0])):
+            self.LessonsClasses[i][1] = int(self.LessonsClasses[i][1].replace("MAT",""))
+        self.LessonsClasses[0][0] = 1
+        j = 0
+        for i in range(1, len(self.LessonsClasses[:,0])):
+            j = j + 1
+            if self.LessonsClasses[i - 1][1] == self.LessonsClasses[i][1]:
+                self.LessonsClasses[i][0] = j + 1  
+            else:
+                self.LessonsClasses[i][0] = 1
+                j = 0
 
     def GenOpus(self):  
         for i in self.edgesnumbers:
@@ -33,14 +45,13 @@ class CurriculumDB:
 
     def GenPagina(self):
         Pagina = []
-        for i in range(0,int(self.LessonsClasses.size/3)):
+        for i in range(0,len(self.LessonsClasses[:,0])):
             Pagina.append([int(self.LessonsClasses[i][0]),int(self.LessonsClasses[i][1])])
         self.cursor.executemany("INSERT INTO PAGINA (id_pagina, id_opus, id_created, dt_created, am_link, ds_gui, pagina_type, pagina_cat) VALUES (?, ?, 1, GETDATE(), 0, NEWID(), NULL, NULL)",Pagina)
         self.cnxn.commit()
-
     def GenLinguaOpus(self):
         LinguaOpus = []
-        for i in range(0,int(self.CoursesDescriptions.size/3)):
+        for i in range(0,len(self.CoursesDescriptions[:,0])):
             LinguaOpus.append([self.CoursesDescriptions[i][0], self.CoursesDescriptions[i][1], self.CoursesDescriptions[i][2]])
         self.cursor.executemany("SET ANSI_WARNINGS OFF; INSERT INTO LINGUA_OPUS (id_opus, cd_lingua, ds_title, ds_tag, ds_content, in_visible, id_created, dt_created) VALUES (?, 'HISPANIA', ?, ?, '', 1, 1, GETDATE())", LinguaOpus)
         self.cursor.executemany("SET ANSI_WARNINGS OFF; INSERT INTO LINGUA_OPUS (id_opus, cd_lingua, ds_title, ds_tag, ds_content, in_visible, id_created, dt_created) VALUES (?, 'BRITANNIA', ?, ?, '', 1, 1, GETDATE())", LinguaOpus)
@@ -49,7 +60,7 @@ class CurriculumDB:
     def GenLinguaPagina(self):
         LinguaPagina = []
         Pagina = []
-        for i in range(0,int(self.LessonsClasses.size/3)):
+        for i in range(0,len(self.LessonsClasses[:,0])):
             Pagina.append([int(self.LessonsClasses[i][0]),int(self.LessonsClasses[i][1])])
         LastLesson = []
         for i in range(0, len(Pagina) - 1):
@@ -64,6 +75,3 @@ class CurriculumDB:
             LinguaPagina.append([int(self.LessonsClasses[i][1]), int(self.LessonsClasses[i][0]), self.LessonsClasses[i][2], LastLessonCourse])
         self.cursor.executemany("INSERT INTO LINGUA_PAGINA (id_opus, id_pagina, cd_lingua, ds_title, ds_content, id_created, dt_created, id_capstone, ds_StartLesson) VALUES (?, ?, 'BRITANNIA', ?, '', 1, GETDATE(), ?, NULL)",LinguaPagina)
         self.cnxn.commit()
-
-        
-
--- Script/DatabaseManager.py
+++ Script/DatabaseManager.py
@@ -1,16 +1,12 @@
 # Add path to the Class folder. All classes can now be imported directly
 from sys import path
 from os import getcwd
+import sys
 
 from networkx.drawing.layout import planar_layout
 path.append(getcwd() + "/../Class")
-from CurriculumGraph import CurriculumGraph
 from CurriculumDB import CurriculumDB
 
-# import plotting utils
-import matplotlib
-matplotlib.use('Qt5Agg')
-import matplotlib.pyplot as plt
 
 # Load additional libraries
 import networkx as nx
@@ -22,38 +18,18 @@ import pyodbc
 # cdb = CurriculumDB(excel_file, 'serverip', 'databasename', 'username', 'password')
 
 cdb = CurriculumDB(r'../Data/FrequencyListOfTopics.xlsx', '127.0.0.1', 'ALICE_DEVELOPMENT', 'ALICE', 'edutronica')
-cg = CurriculumGraph(r'../Data/FrequencyListOfTopics.xlsx')
 
-# print(cdb.LessonsClasses[0][1])
-# for i in range(1,int(cdb.LessonsClasses.size/3)):
-#     if int(cdb.LessonsClasses[i][1]) != int(cdb.LessonsClasses[i-1][1]):
-#         print(int(cdb.LessonsClasses[i][1]))
+# cdb.cursor.execute('DELETE FROM LINGUA_PAGINA')
+# cdb.cursor.execute('DELETE FROM LINGUA_OPUS')
+# cdb.cursor.execute('DELETE FROM PAGINA')
+# cdb.cursor.execute('DELETE FROM OPUS')
+# cdb.cnxn.commit()
 
 # cdb.GenOpus()
 # cdb.GenPagina()
 # cdb.GenLinguaPagina()
 # cdb.GenLinguaOpus()
 
-# cdb.cursor.execute('DELETE FROM INTEREST')
-# cdb.cursor.execute('DELETE FROM CATEGORY')
-# cdb.cursor.execute('DELETE FROM LINGUA_PAGINA')
-# cdb.cursor.execute('DELETE FROM LINGUA_OPUS')
-# cdb.cursor.execute('DELETE FROM PAGINA')
-# cdb.cursor.execute('DELETE FROM OPUS')
-
-# cdb.cnxn.commit()
-
-cdb.cursor.execute('SELECT * FROM LINGUA_PAGINA')
-for row in cdb.cursor:
-    print(row)
-
-# print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-
-# for i in cdb.edgesnumbers:
-#     print(i)
-
-# print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-
-# for i in cg.SheetNameEdgesList:
-#     print(i)
-
+# cdb.cursor.execute('SELECT * FROM PAGINA')
+# for row in cdb.cursor:
+#     print(row)
